47fefc8629ecdccf3edcf7304e8fbe464f703098
embo <emma.hakonsen@gmail.com>
2022-10-01 16:12:30 +0200

    Har endret koden i oppgave 2b slik at den bestå test 2n.

--- src/main/java/no/oslomet/cs/algdat/Oblig2/DobbeltLenketListe.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig2/DobbeltLenketListe.java
@@ -39,16 +39,16 @@ public class DobbeltLenketListe<T> implements Liste<T> {
 
     public DobbeltLenketListe(T[] a) {
        Objects.requireNonNull(a,"Tabellen a er null!");
-       hode = hale = null;      //initialisere med null-verdier
+       hode = hale = null;                          //initialisere med null-verdier
        for (int i = 0; i < a.length; i++) {
-           if (a[i] != null) {      //tar ikke med null-verdier fra tabellen a
-              Node node = new Node(a[i]);        //oppretter node med verdi, ikke festet til noe ennå
+           if (a[i] != null) {                      //tar ikke med null-verdier fra tabellen a
+              Node node = new Node(a[i]);           //oppretter node med verdi, ikke festet til noe ennå
               if (hode == null) {
-                  hode = hale = node;        //første node i lenket liste
+                  hode = hale = node;               //første node i lenket liste
               } else {
-                  hale.neste = node;     //første halen peker mot ny node
-                  node.forrige = hale;   //ny node peker tilbake til forrige hale
-                  hale = node;           //flytter halen til ny node
+                  hale.neste = node;                //første halen peker mot ny node
+                  node.forrige = hale;              //ny node peker tilbake til forrige hale
+                  hale = node;                      //flytter halen til ny node
               }
            }
        }
@@ -77,14 +77,18 @@ public class DobbeltLenketListe<T> implements Liste<T> {
     
     public static void main(String[] args) {
     
-        String[] s1 = {}, s2 = {"A"}, s3 = {null,"A",null,"B",null};
-        DobbeltLenketListe<String> l1 = new DobbeltLenketListe<>(s1);
-        DobbeltLenketListe<String> l2 = new DobbeltLenketListe<>(s2);
-        DobbeltLenketListe<String> l3 = new DobbeltLenketListe<>(s3);
-        System.out.println(l1.toString() + " " + l2.toString()
-                + " " + l3.toString() + " " + l1.omvendtString() + " "
-                + l2.omvendtString() + " " + l3.omvendtString());
-        // Utskrift: [] [A] [A, B] [] [A] [B, A]
+        DobbeltLenketListe<Integer> liste = new DobbeltLenketListe<>();
+        System.out.println(liste.toString() + " " + liste.omvendtString());
+        for (int i = 1; i <= 3; i++) {
+            liste.leggInn(i);
+            System.out.println(liste.toString() + " " + liste.omvendtString());
+        }
+        // Utskrift:
+        // [] []
+        // [1] [1]
+        // [1, 2] [2, 1]
+        // [1, 2, 3] [3, 2, 1]
+    
     
     }
 
@@ -147,29 +151,30 @@ public class DobbeltLenketListe<T> implements Liste<T> {
 
     @Override
     public String toString() {
-        Node current = hode;            //starter med hode
-        if (tom()) {
+        Node current = hode;                                //starter med hode
+        StringBuilder output = new StringBuilder("[");      //initialiserer Stringbuilder-variabel
+        if (tom()) {                                        //håndterer tomme lister
             return "[]";
         } else {
-            StringBuilder output = new StringBuilder("[");     //initialiserer tom StringBuilder-variabel
-            for (int i = 0; i < antall() - 1; i++) {          //looper gjennom listen
-                output.append(current.verdi + ", ");           //verdien i hver node legges til StringBuilder-variabelen
-                current = current.neste;                //flytter til neste node
+            while (current.neste != null) {                 //looper gjennom listen til vi når den siste noden
+                output.append(current.verdi + ", ");        //verdien i hver node legges til StringBuilder-variabelen
+                current = current.neste;                    //flytter til neste node
             }
             output.append(current.verdi + "]");
             return output.toString();
         }
+        
     }
 
     public String omvendtString() {
-        Node current = hale;            //starter med hale
-        if (tom()) {
+        Node current = hale;                                //starter med hale
+        StringBuilder output = new StringBuilder("[");      //initialiserer Stringbuilder-variabel
+        if (tom()) {                                        //håndterer tomme lister
             return "[]";
         } else {
-            StringBuilder output = new StringBuilder("[");     //initialiserer tom StringBuilder-variabel
-            for (int i = antall() - 1; i > 0; i--) {             //looper gjennom listen baklengs
-                output.append(current.verdi + ", ");           //verdien i hver node legges til StringBuilder-variabelen
-                current = current.forrige;          //flytter til forrige node
+            while (current.forrige != null) {               //looper gjennom listen baklengs til vi når den første noden
+                output.append(current.verdi + ", ");        //verdien i hver node legges til StringBuilder-variabelen
+                current = current.forrige;                  //flytter til forrige node
             }
             output.append(current.verdi + "]");
             return output.toString();
